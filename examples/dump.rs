e9504c47c7bdf0e958da5ba10955ccecf920469a
Chris West (Faux) <git@goeswhere.com>
2018-08-30 22:56:36 +0100

    example runner

---
+++ examples/dump.rs
@@ -0,0 +1,21 @@
+#[macro_use]
+extern crate failure;
+extern crate heifers;
+
+use std::env;
+use std::fs;
+use std::io;
+
+use failure::Error;
+
+fn main() -> Result<(), Error> {
+    println!(
+        "{:?}",
+        heifers::open(io::BufReader::new(fs::File::open(
+            env::args_os()
+                .nth(1)
+                .ok_or_else(|| format_err!("usage: filename"))?
+        )?))?
+    );
+    Ok(())
+}
