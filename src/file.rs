a9cce5791e1b2e25993bfa4330da09a0d7c2464a
Chris West (Faux) <git@goeswhere.com>
2018-09-02 21:40:11 +0100

    horribly mangle the extent algo as usual

--- src/file.rs
+++ src/file.rs
@@ -1,14 +1,19 @@
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::io;
 use std::io::Read;
+use std::io::Seek;
+use std::io::SeekFrom;
 
 use cast::u32;
+use cast::u64;
 use cast::usize;
 use failure::Error;
 
 use mpeg;
 use mpeg::iprp::Property;
+use mpeg::Extent;
 use mpeg::FourCc;
 use mpeg::ItemInfo;
 use mpeg::ItemLoc;
@@ -96,6 +101,89 @@ impl Heif {
             items,
             props,
         })
+    }
+
+    pub fn primary_item_id(&self) -> u32 {
+        self.primary_item
+    }
+
+    pub fn open_item_data<R: Read + Seek>(
+        &self,
+        mut from: R,
+        item: u32,
+    ) -> Result<Extents<R>, Error> {
+        let item = self
+            .items
+            .get(&item)
+            .ok_or_else(|| format_err!("invalid item id"))?;
+
+        let first_extent = &item
+            .location
+            .extents
+            .get(0)
+            .ok_or_else(|| format_err!("empty extents"))?;
+
+        ensure!(0 != first_extent.length, "empty first extent");
+        ensure!(0 == first_extent.index, "empty first extent");
+
+        from.seek(SeekFrom::Start(
+            item.location.base_offset + first_extent.offset,
+        ))?;
+
+        Ok(Extents {
+            inner: from,
+            base: item.location.base_offset,
+            extents: &item.location.extents,
+            current_extent: 0,
+            current_pos: 0,
+        })
+    }
+}
+
+pub struct Extents<'h, R> {
+    inner: R,
+    base: u64,
+    extents: &'h [Extent],
+    current_extent: usize,
+    current_pos: u64,
+}
+
+impl<'h, R: Read + Seek> Read for Extents<'h, R> {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, io::Error> {
+        if buf.is_empty() {
+            return Ok(0);
+        }
+
+        if self.extents.len() == self.current_extent {
+            return Ok(0);
+        }
+
+        let current = &self.extents[self.current_extent];
+
+        assert_lt!(self.current_pos, current.length);
+        let to_read = usize((current.length - self.current_pos).min(u64(buf.len())));
+
+        let actually_read = self.inner.read(&mut buf[..to_read])?;
+
+        self.current_pos += u64(actually_read);
+
+        if self.current_pos == current.length {
+            self.current_extent += 1;
+
+            if self.current_extent != self.extents.len() {
+                let new_extent = &self.extents[self.current_extent];
+                assert_eq!(
+                    new_extent.index,
+                    u64(self.current_extent),
+                    "extent index out of sequence",
+                );
+
+                self.inner
+                    .seek(SeekFrom::Start(self.base + new_extent.offset))?;
+            }
+        }
+
+        Ok(actually_read)
     }
 }
 
--- tests/smoke.rs
+++ tests/smoke.rs
@@ -3,14 +3,20 @@ extern crate failure;
 extern crate heifers;
 
 use std::io::Cursor;
+use std::io::Read;
 
 use failure::Error;
 
 #[test]
 fn road() -> Result<(), Error> {
-    println!(
-        "{:?}",
-        heifers::Heif::new(Cursor::new(&include_bytes!("data/road.heic")[..]))?
-    );
+    let bytes = &include_bytes!("data/road.heic")[..];
+    let mut file = Cursor::new(bytes);
+    let heif = heifers::Heif::new(&mut file)?;
+    println!("{:?}", heif);
+    let mut data = Vec::new();
+    heif.open_item_data(file, heif.primary_item_id())?
+        .read_to_end(&mut data)?;
+
+    assert_eq!(&bytes[333..], data.as_slice());
     Ok(())
 }
